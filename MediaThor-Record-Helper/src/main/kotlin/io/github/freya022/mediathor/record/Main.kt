f0f616156cff79a564fa7e0b9ebafccb846391ee
freya02 <41875020+freya022@users.noreply.github.com>
2023-08-20 21:06:33 +0200

    Disable test code

--- MediaThor-Record-Helper/src/main/kotlin/io/github/freya022/mediathor/record/Main.kt
+++ MediaThor-Record-Helper/src/main/kotlin/io/github/freya022/mediathor/record/Main.kt
@@ -3,9 +3,6 @@ package io.github.freya022.mediathor.record
 import com.github.jnrwinfspteam.jnrwinfsp.service.ServiceRunner
 import io.github.freya022.mediathor.record.memfs.FSMain
 import io.github.freya022.mediathor.record.memfs.WinFspMemFS
-import kotlin.concurrent.thread
-import kotlin.io.path.Path
-import kotlin.io.path.copyTo
 
 object Main {
     private const val GB = 1024 * 1024 * 1024
@@ -16,13 +13,13 @@ object Main {
 
         RecordWatcher(memFS)
 
-        thread {
-            Thread.sleep(1000)
-
-            Path("C:\\Users\\freya02\\Videos\\Replay 2023-08-11 18-22-53.mkv").copyTo(memFS.mountPointPath.resolve("first.mkv"))
-            Path("C:\\Users\\freya02\\Videos\\Replay 2023-08-11 18-23-04.mkv").copyTo(memFS.mountPointPath.resolve("second.mkv"))
-            Path("C:\\Users\\freya02\\Videos\\Replay 2023-08-12 16-49-27.mkv").copyTo(memFS.mountPointPath.resolve("unrelated.mkv"))
-        }
+//        thread {
+//            Thread.sleep(1000)
+//
+//            Path("C:\\Users\\freya02\\Videos\\Replay 2023-08-11 18-22-53.mkv").copyTo(memFS.mountPointPath.resolve("first.mkv"))
+//            Path("C:\\Users\\freya02\\Videos\\Replay 2023-08-11 18-23-04.mkv").copyTo(memFS.mountPointPath.resolve("second.mkv"))
+//            Path("C:\\Users\\freya02\\Videos\\Replay 2023-08-12 16-49-27.mkv").copyTo(memFS.mountPointPath.resolve("unrelated.mkv"))
+//        }
 
         ServiceRunner.mountLocalDriveAsService("OBS Volatile FS", memFS, FSMain.root)
     }
