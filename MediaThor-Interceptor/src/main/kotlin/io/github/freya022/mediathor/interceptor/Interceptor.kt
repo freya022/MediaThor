5290f9e366f7b842ed0df176ccfdb35dc27f4ab6
freya02 <41875020+freya022@users.noreply.github.com>
2023-09-05 16:02:42 +0200

    Try to improve the shutdown process

--- MediaThor-Interceptor/src/main/kotlin/io/github/freya022/mediathor/interceptor/Interceptor.kt
+++ MediaThor-Interceptor/src/main/kotlin/io/github/freya022/mediathor/interceptor/Interceptor.kt
@@ -5,29 +5,41 @@ import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.openqa.selenium.Platform
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.devtools.v116.network.Network
+import org.openqa.selenium.devtools.v116.target.Target
 import org.openqa.selenium.edge.EdgeDriver
+import org.openqa.selenium.remote.DriverCommand
+import org.openqa.selenium.remote.RemoteWebDriver
 import java.nio.file.StandardOpenOption
 import java.util.*
 import java.util.logging.Level
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 import kotlin.io.path.Path
 import kotlin.io.path.bufferedWriter
+import kotlin.jvm.optionals.getOrNull
 
 private val logger = KotlinLogging.logger { }
 
-class Interceptor {
+class Interceptor private constructor() {
     private val writer = outputPath.bufferedWriter(options = arrayOf(StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING))
 
-    init {
-        val driver = when (Platform.getCurrent().family()) {
-            Platform.WINDOWS -> EdgeDriver()
-            else -> ChromeDriver()
-        }
-        driver.setLogLevel(Level.INFO)
+    private lateinit var continuation: Continuation<Unit>
 
-        Runtime.getRuntime().addShutdownHook(Thread {
-            writer.close()
-            driver.quit()
-        })
+    private val driver = when (Platform.getCurrent().family()) {
+        Platform.WINDOWS -> EdgeDriver()
+        else -> ChromeDriver()
+    }
+
+    private val hook = Thread {
+        driver.maybeGetDevTools().getOrNull()?.clearListeners()
+        driver.quit()
+        close()
+    }
+
+    init {
+        driver.setLogLevel(Level.INFO)
+        Runtime.getRuntime().addShutdownHook(hook)
 
         val devTools = driver.devTools
         devTools.createSession()
@@ -48,9 +60,35 @@ class Interceptor {
             logger.info { "Added $url" }
             writer.appendLine(url)
         }
+
+        devTools.addListener(Target.detachedFromTarget()) {
+            logger.info { "Exiting" }
+            Runtime.getRuntime().removeShutdownHook(hook)
+            continuation.resume(Unit)
+        }
+    }
+
+    private suspend fun awaitTermination(): Unit = suspendCoroutine {
+        continuation = it
+    }
+
+    // This method is only used when the browser is already closed.
+    private fun close() {
+        writer.close()
     }
 
     companion object {
         val outputPath = Path("master_playlists.txt")
+
+        suspend fun intercept() {
+            val interceptor = Interceptor()
+            interceptor.awaitTermination()
+            // If the browser is closed, the driver is still running
+            // Using the QUIT command makes it exit
+            val executeMethod = RemoteWebDriver::class.java.getDeclaredMethod("execute", String::class.java)
+            executeMethod.isAccessible = true
+            executeMethod.invoke(interceptor.driver, DriverCommand.QUIT)
+            interceptor.close()
+        }
     }
 }
--- MediaThor-Interceptor/src/main/kotlin/io/github/freya022/mediathor/interceptor/InterceptorMain.kt
+++ MediaThor-Interceptor/src/main/kotlin/io/github/freya022/mediathor/interceptor/InterceptorMain.kt
@@ -1,5 +1,6 @@
 package io.github.freya022.mediathor.interceptor
 
+import kotlinx.coroutines.runBlocking
 import java.io.IOException
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.exists
@@ -10,8 +11,8 @@ object InterceptorMain {
         if (Interceptor.outputPath.exists() && "-y" !in args)
             throw IOException("${Interceptor.outputPath.absolutePathString()} already exists")
 
-        Interceptor()
-
-        Thread.sleep(Long.MAX_VALUE)
+        runBlocking {
+            Interceptor.intercept()
+        }
     }
 }
